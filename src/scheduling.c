32f5ee03f6d773c37b55f918757617279d071da8
suter <frederic.suter@cc.in2p3.fr>
2012-10-12 14:32:38 +0200

    more comments

--- src/scheduling.c
+++ src/scheduling.c
@@ -52,11 +52,25 @@ alg_t getAlgorithmByName(char* name) {
   }
 }
 
+
+/* When some independent tasks are scheduled on the same resource, the SimGrid
+ * kernel start them in parallel as soon as possible even though the scheduler
+ * assumed a sequential execution. This function addresses this issue by
+ * enforcing that sequential execution wanted by the scheduler. A resource
+ * dependency is added to that extent.
+ */
 void handle_resource_dependency(SD_workstation_t workstation, SD_task_t task){
+  /* Get the last task executed on this workstation */
   SD_task_t source = SD_workstation_get_last_scheduled_task(workstation);
 
+  /* If such a task exists, is still in the system (scheduled or running) and is
+   * not already a predecessor of the current task, create a resource dependency
+   */
   if (source && (SD_task_get_state(source)!= SD_DONE) &&
       !SD_task_dependency_exists(source, task))
     SD_task_dependency_add("resource", NULL, source, task);
+
+  /* update the information on what is the last task executed on this
+   * workstation */
   SD_workstation_set_last_scheduled_task(workstation, task);
 }
