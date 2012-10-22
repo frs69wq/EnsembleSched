d70f7052e6ad0cb55995c2e74ed74ac80e32b077
suter <frederic.suter@cc.in2p3.fr>
2012-10-22 15:01:56 +0200

    Detect the completion of a DAX. Add extra stopping conditions to stop when everything is done. More comments to explain that.

--- src/dpds.c
+++ src/dpds.c
@@ -76,6 +76,7 @@ void dpds_provision(double c, double t, int nVM, scheduling_globals_t globals){
 void dpds_schedule(xbt_dynar_t daxes, scheduling_globals_t globals){
   unsigned int i, j;
   int first_call = 1, step = 1;
+  int completed_daxes = 0;
   xbt_dynar_t priority_queue;
   xbt_dynar_t idleVMs = NULL;
   xbt_dynar_t ready_children = NULL;
@@ -101,89 +102,108 @@ void dpds_schedule(xbt_dynar_t daxes, scheduling_globals_t globals){
    */
   xbt_dynar_sort(priority_queue, daxPriorityCompareTasks);
 
-  /* Main scheduling loop: lines 7 to 16 */
-  /* Remark: order is changed w.r.t to the article (lines 13-15 then lines 8-12)
-   * but not the behavior as there is a specific first call.
-   */
   do{
-  while (first_call || (step*globals->period - SD_get_clock())<0.00001 ||
-      !xbt_dynar_is_empty((changed =
-          SD_simulate(MIN(step*globals->period,
-              globals->deadline)-SD_get_clock())))){
-    /* Apart of the first specific call, the simulation is suspended when
-     *  - a watchpoint is reached, meaning a compute task has finished
-     *  - the deadline is met
+    /* Main scheduling loop: lines 7 to 16 */
+    /* Remark: order is changed w.r.t to the article (lines 13-15 then lines 8-12)
+     * but not the behavior as there is a specific first call.
      */
-    XBT_INFO("%.3f %.3f",step*globals->period,
-        step*globals->period -SD_get_clock());
-    /* Handling specific stopping conditions */
-    first_call=0;
-    if (((step*globals->period)-SD_get_clock())<0.00001){
-      XBT_INFO("End of 1h period. Start a new one");
-      step++;
-      continue;
-    }
-    if (globals->deadline <= SD_get_clock()){
-      XBT_INFO("Time's up! Deadline was reached at %.3f", SD_get_clock());
-      break;
-    }
 
-    /* Typical loop body*/
-    /* Action on completion of a task (lines 13 to 15) */
+    /* This do-while external loop is to ensure that we call the main scheduling
+     * loop again even though no event occurred during a complete provisioning
+     * period.
+     */
+    while (first_call ||
+           ((completed_daxes < xbt_dynar_length(daxes)) &&
+           ((step*globals->period - SD_get_clock())<0.00001 ||
+            !xbt_dynar_is_empty((changed = SD_simulate(MIN(step*globals->period,
+                                                           globals->deadline) -
+                                                       SD_get_clock())))))){
+      /* Apart of the first specific call, the simulation is suspended when
+       *  - All the DAXes are done (no more work to be done, why continue?)
+       *  - a watch point is reached, meaning a compute task has finished
+       *  - a provisioning period has ended
+       *  - the deadline is met
+       */
+      /* Handling specific stopping conditions */
+      first_call=0;
+      if (((step*globals->period)-SD_get_clock())<0.00001){
+        XBT_DEBUG("End of a period of %.0f seconds. Start a new one",
+            globals->period);
+        step++;
+        continue;
+      }
+      if (globals->deadline <= SD_get_clock()){
+        XBT_INFO("Time's up! Deadline was reached at %.3f", SD_get_clock());
+        break;
+      }
 
-    xbt_dynar_foreach(changed, i, t){
-       if (SD_task_get_kind(t) == SD_TASK_COMP_SEQ &&
-           SD_task_get_state(t) == SD_DONE){
-         XBT_VERB("%s (from %s) has completed", SD_task_get_name(t),
-             SD_task_get_dax_name(t));
+      /* Typical loop body*/
+      /* Action on completion of a task (lines 13 to 15) */
 
-         /* get the workstation used to compute this task */
-         v = (SD_task_get_workstation_list(t))[0];
+      xbt_dynar_foreach(changed, i, t){
+        if (SD_task_get_kind(t) == SD_TASK_COMP_SEQ &&
+            SD_task_get_state(t) == SD_DONE){
+          XBT_VERB("%s (from %s) has completed", SD_task_get_name(t),
+              SD_task_get_dax_name(t));
 
-         /* Set it to idle and add it the list of idle VMs */
-         SD_workstation_set_to_idle(v);
-         xbt_dynar_push(idleVMs, &v);
+          /* get the workstation used to compute this task */
+          v = (SD_task_get_workstation_list(t))[0];
 
-         /* add ready children of t to the priority queue */
-         ready_children = SD_task_get_ready_children(t);
-         xbt_dynar_foreach(ready_children, j, child){
-           /* Ensure that a task is not added more than once to the queue. May
-            * occur as soon as a task as more than one parent. */
-           if (!xbt_dynar_member(priority_queue, &child))
-             xbt_dynar_push(priority_queue, &child);
-         }
-         xbt_dynar_free_container(&ready_children); /* avoid memory leaks */
-       }
-    }
-    xbt_dynar_free_container(&changed); /* avoid memory leaks */
+          /* Set it to idle and add it the list of idle VMs */
+          SD_workstation_set_to_idle(v);
+          xbt_dynar_push(idleVMs, &v);
 
-    /* Sort again the priority queue as new tasks have been added. */
-    xbt_dynar_sort(priority_queue, daxPriorityCompareTasks);
-    /* Display the current contents of the priority queue as debug information*/
-    xbt_dynar_foreach(priority_queue,j,child)
+          /* Detection of the completion of a workflow */
+          if (!strcmp(SD_task_get_name(t), "end")){
+            XBT_DEBUG("DAX %s has completed", SD_task_get_dax_name(t));
+            completed_daxes++;
+          }
+          /* add ready children of t to the priority queue */
+          ready_children = SD_task_get_ready_children(t);
+          xbt_dynar_foreach(ready_children, j, child){
+            /* Ensure that a task is not added more than once to the queue. May
+             * occur as soon as a task as more than one parent. */
+            if (!xbt_dynar_member(priority_queue, &child))
+              xbt_dynar_push(priority_queue, &child);
+          }
+          xbt_dynar_free_container(&ready_children); /* avoid memory leaks */
+        }
+      }
+      xbt_dynar_free_container(&changed); /* avoid memory leaks */
+
+      /* Sort again the priority queue as new tasks have been added. */
+      xbt_dynar_sort(priority_queue, daxPriorityCompareTasks);
+      /* Display the current contents of the priority queue as debug information*/
+      xbt_dynar_foreach(priority_queue,j,child)
       XBT_DEBUG("%s is in priority queue", SD_task_get_name(child));
 
-    /* Task scheduling part (lines 8 to 12) */
-    while ((!xbt_dynar_is_empty(idleVMs)) &&
-        (!xbt_dynar_is_empty(priority_queue))){
+      /* Task scheduling part (lines 8 to 12) */
+      while ((!xbt_dynar_is_empty(idleVMs)) &&
+          (!xbt_dynar_is_empty(priority_queue))){
 
-      /* Remove a random VM from the list of idleVMs and set it as busy */
-      v = select_random(idleVMs);
-      SD_workstation_set_to_busy(v);
+        /* Remove a random VM from the list of idleVMs and set it as busy */
+        v = select_random(idleVMs);
+        SD_workstation_set_to_busy(v);
 
-      /* Pop the last task from the queue, i.e. one belonging to the DAX of
-       * highest priority. */
-      xbt_dynar_pop(priority_queue, &t);
+        /* Pop the last task from the queue, i.e. one belonging to the DAX of
+         * highest priority. */
+        xbt_dynar_pop(priority_queue, &t);
 
-      XBT_VERB("Schedule %s (%s) on %s", SD_task_get_name(t),
-          SD_task_get_dax_name(t),
-          SD_workstation_get_name(v));
+        XBT_VERB("Schedule %s (%s) on %s", SD_task_get_name(t),
+            SD_task_get_dax_name(t),
+            SD_workstation_get_name(v));
 
-      SD_task_schedulel(t, 1, v);
-      handle_resource_dependency(v, t);
-     }
-  }
-  } while (SD_get_clock()< globals->deadline);
+        SD_task_schedulel(t, 1, v);
+        handle_resource_dependency(v, t);
+      }
+    }
+  } while ((SD_get_clock()< globals->deadline) &&
+           (completed_daxes < xbt_dynar_length(daxes)));
+
+  XBT_INFO("Simulation is complete after %f seconds."
+      " %d DAXes have completed among %lu.", SD_get_clock(), completed_daxes,
+      xbt_dynar_length(daxes));
+
   /* Cleaning step once simulation is over */
   xbt_dynar_free_container(&idleVMs);
   xbt_dynar_free_container(&priority_queue);
